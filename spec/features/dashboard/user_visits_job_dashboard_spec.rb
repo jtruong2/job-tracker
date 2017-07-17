2a78b365f12a88e0cc2c95aa25144e00f1459c59
jtruong2 <jtruong2@comcast.net>
2017-07-16 22:40:14 -0600

    fix tests

--- spec/features/dashboard/user_visits_job_dashboard_spec.rb
+++ spec/features/dashboard/user_visits_job_dashboard_spec.rb
@@ -1,42 +1,19 @@
 require 'rails_helper'
-RSpec.describe "User visits /jobs"
-  scenario "sees a list of jobs sorted by city" do
-    job1 = create(:job)
-    job2 = create(:job, city: "Seattle")
-    job3 = create(:job, city: "Miami")
-
-    visit '/jobs'
-    select("location", from: "sort").select_option
-    click_on "filter"
-
-    expect(page).to
-  end
-
-  scenario "sees a list of jobs sorted by interest" do
-
-  end
-
+RSpec.describe "User visits /dashboard" do
   scenario "sees jobs with count by level of interest" do
-    job1, job2 = create(:job)
+    job1, job2 = create_list(:job, 2)
 
     visit '/dashboard'
 
-    expect(page).to have_content("45: 60")
-  end
-
-  scenario "sees top three companies ranked by level of interest with average" do
-    job1, job2, job3, job4 = create_list(:job)
-
-    visit '/dashboard'
-
-    expect(page).to
+    expect(page).to have_content("level 60: 2")
   end
 
   scenario "sees job count by location" do
-    job1, job2, job3 = create_list(:job)
+    job1, job2, job3 = create_list(:job, 3)
 
     visit '/dashboard'
 
     expect(page).to have_content("Denver: 3")
+    expect(page).to have_link("Denver", href: "/jobs?location=Denver")
   end
 end
